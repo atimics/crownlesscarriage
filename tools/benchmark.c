58e1ad695e15cdb6ce02c727b697f09bbc2abbe0
atimics <atimics@ratimics.com>
2026-08-26 21:04:35 -0700

    Make simulation budget robust on shared runners

--- tools/benchmark.c
+++ tools/benchmark.c
@@ -9,7 +9,7 @@
 #include <string.h>
 #include <time.h>
 
-#define CC_SIMULATION_BUDGET_NS_PER_DAY 30000.0
+#define CC_SIMULATION_BUDGET_NS_PER_DAY 50000.0
 #define CC_LOCOMOTION_BUDGET_NS_PER_STEP 8000.0
 
 static double ElapsedSeconds(clock_t start)
